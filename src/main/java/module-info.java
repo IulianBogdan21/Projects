80274df9a0ec53efc5ca05ea43b71e977a63be0e
AndreiHokage <andrei.balanici@yahoo.com>
2021-12-07 21:52:50 +0200

    Solved JAVAFX

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module socialNetwork.ui {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
+
+    opens socialNetwork.ui to javafx.fxml;
+    exports socialNetwork.ui;
+
+}
---
+++ src/main/java/socialNetwork/ui/HelloController.java
@@ -0,0 +1,9 @@
+package socialNetwork.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class HelloController {
+    @FXML
+    Button button;
+}
