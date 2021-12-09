1ee07a555566274dd59e2c999d29a9bed8932f33
AndreiHokage <andrei.balanici@yahoo.com>
2021-12-09 20:22:20 +0200

    lab11_LogIN_ForUI

---
+++ src/main/java/socialNetwork/exceptions/LogInException.java
@@ -0,0 +1,12 @@
+package socialNetwork.exceptions;
+
+public class LogInException extends ExceptionBaseClass{
+    /**
+     * constructor
+     *
+     * @param message - String
+     */
+    public LogInException(String message) {
+        super(message);
+    }
+}
